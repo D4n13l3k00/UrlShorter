af593da8e3764b6491762a0b993d998bd6d37197
D4n13l3k00 <D4n13l3k00@yandex.ru>
2021-09-24 17:43:16 +0300

    mini refactor

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
+import os
 import random
 import re
 import string
-import os
+
 import aioredis
 from fastapi import FastAPI, Request
-from fastapi.responses import *
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 
-db = aioredis.from_url(os.getenv("REDIS_URL") or "redis://localhost/1", decode_responses=True)
+db = aioredis.from_url(os.getenv("REDIS_URL")
+                       or "redis://localhost/1", decode_responses=True)
 
 templates = Jinja2Templates(directory="templates")
 is_url = re.compile(
     r'((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*')
 
 app = FastAPI(
-    title='Shrtr',
-    version='1.0'
+    title='UrlShorter',
+    version='1.1',
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None
 )
 
-@app.get('/', include_in_schema=False)
-async def main_html(rq: Request):
-    return templates.TemplateResponse("index.html", {"request":rq})
 
-@app.get('/create', include_in_schema=False)
+@app.get('/')
+async def main_html(rq: Request):
+    return templates.TemplateResponse("index.html", {"request": rq})
+
+
+@app.get('/create')
 async def create(rq: Request, link: str):
     if not is_url.match(link):
-        return templates.TemplateResponse("message.html", {"request":rq, "message":"Not a valid url"}, 502)
+        return templates.TemplateResponse("message.html", {"request": rq, "message": "Not a valid url"}, 404)
     n = 1
     while True:
-        shrt = ''.join(random.sample(string.ascii_letters, n))
+        shrt = ''.join(random.choice(string.ascii_letters) for _ in range(n))
         if not await db.get(shrt):
             break
         n += 1
     await db.set(shrt, link)
-    return templates.TemplateResponse("success.html", {"request":rq, "link":f"http://{rq.url.hostname}{':'+ str(rq.base_url.port) if rq.base_url.port else ''}/{shrt}"})
+    return templates.TemplateResponse("success.html",
+                                      {"request": rq,
+                                       "link": f"http://{rq.url.hostname}{':'+ str(rq.base_url.port) if rq.base_url.port else ''}/{shrt}"
+                                       })
 
 
-@app.get('/{shrt}', include_in_schema=False)
+@app.get('/{shrt}')
 async def create(rq: Request, shrt: str):
     u = await db.get(shrt)
     if u:
         return RedirectResponse(u)
-    return templates.TemplateResponse("message.html", {"request":rq, "message":"404: Not found"}, 404)
-
+    return templates.TemplateResponse("message.html", {"request": rq, "message": "404: Not found"}, 404)
